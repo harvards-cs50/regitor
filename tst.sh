a54491646ea6b8baefc188e97505532a18c2f8b7
harvards-cs50 <cs50.harvards@gmail.com>
2021-11-29 22:00:28 +0530

    upload

---
+++ tst.sh
@@ -0,0 +1,40 @@
+tempdir=$(openssl rand -hex 12)
+mkdir $tempdir
+
+keys=(
+    name 
+    port 
+    cert 
+    auth 
+    username 
+    password
+)
+
+conditions=(
+    '[[ "$value" =~ [a-zA-Z0-9]+$ && ! -d "$value" ]]'
+    '[[ "$value" =~ [0-9]:[0-9]+$ ]]'
+    '[[ "(y ye yes n no)" =~ "${value,,}" ]]'
+    '[[ "(y ye yes n no)" =~ "${value,,}" ]]'
+    '[[ "$value" =~ [a-zA-Z0-9]+$ && "$auth" == "yes" ]]'
+    '[[ "$value" =~ [a-zA-Z0-9]+$ && "$auth" == "yes" ]]'
+)
+
+commands=(
+    'mv $tempdir "$value";'
+    'echo -e "docker run -d -p $port --restart=always --name $name" >> $name/executor.txt;'
+    'if [[ "(y ye yes)" =~ ${value,,} ]]; then cert="yes"; bash lib/create_cert.sh $name; else cert="no"; fi'
+    'if [[ "(y ye yes)" =~ ${value,,} ]]; then auth="yes"; else auth="no"; fi'
+    ''
+    'bash lib/create_auth.sh $name $username $password'
+)
+
+
+for i in ${!keys[@]}; 
+do
+    value=$(grep -x "${keys[i]}.*" details.txt | tail -1 | sed 's/ //g' | tr "=>" " " | cut -d " " -f 2)
+    until eval ${conditions[i]}; do read -p "Enter ${keys[i]} : " value; done
+    eval "${keys[i]}=$value"
+    eval ${commands[i]}
+done
+
+echo -e "name = $name\nport = $port\ncert = $cert\nauth = $auth\nusername = $username\npassword = $password" >> $name/data.txt
