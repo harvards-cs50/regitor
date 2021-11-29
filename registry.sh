c979e0178416929fcfa6065c229e3df8bd77e62d
harvards-cs50 <cs50.harvards@gmail.com>
2021-11-29 22:29:55 +0530

    upload

--- registry.sh
+++ registry.sh
@@ -1,41 +1,14 @@
-# Funtion Calling
-#   => place "-f" option before the name of function in cmd (command line)
-#       => Eg. >>> registry -f new-registry [filename]  --- (filename <- optional)
-#       =>     Note = First source this file to run it in this way
-
-
-#file format
-#   => Sequence not necessary
-#   => Every pair of key-value should be in new line
-#   => Seperators allows "=" and ">"
-#         => key = value
-#         => key > value
-#   => Keys are required for according to functions
-#         => new-registry
-#              KEYS        Values
-#              name     =  Give your custom name to the registry
-#              port     =  Eg. 5000:5000 , 1234:5000 , etc
-#              auth     =  0 or 1  => 0 = (no password  ; 1 means put password) to registry
-#              username =  Username for auth login ; Not needed if auth is 0
-#              password =  Password for auth login ; Not needed if auth is 0
-#              cert     =  0 or 1  => 0 = (no certificate  ; 1 means put certificate) to registry
-#
+cpath=$(dirname "$0")
 
 options=(
     -nr
 )
 
-filerun=(
-    lib/new-registry
+commands=(
+    'bash $cpath/lib/new-registry.sh'
 )
 
-args=("$@")
-
-for arg in $(seq ${#args[@]});
+for i in ${!options[@]};
 do
-    if [[ "$1" == "registry" ]]; then
-        if [[ "${args[arg-1]}" == "-f" ]]; then
-            ${args[arg]} ${args[arg+1]}
-        fi
-    fi
+    if [[ "$1" == "${options[i]}" ]]; then eval ${commands[i]}; fi
 done
